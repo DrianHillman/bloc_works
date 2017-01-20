e6814e635a2bdeea506ac6ecd31fa2be9a3b192b
Drian Hillman <dhillman24@gmail.com>
2017-01-20 00:51:24 +0000

    Initialized gem structure & overview of framework architecture | Checkpoint 1 Complete :white_check_mark:

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -1,5 +1,9 @@
 require "bloc_works/version"
 
 module BlocWorks
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
+    end
+  end
 end
